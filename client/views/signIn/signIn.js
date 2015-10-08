642e8bb52000c55a20b0179041069ee65e31f016
ndunhourowens <ndunhour@hotmail.com>
2015-10-07 21:26:48 -1000

    create a signIn.js to code various situation which revolves around successful/non-sucessful login attempts

---
+++ client/views/signIn/signIn.js
@@ -0,0 +1,17 @@
+// New user -create account
+
+  // successful login ==> dashboard.html
+
+  // failed creation ==> retry
+
+
+
+// Returning user - sign in
+
+  // successful login ==> dashboard.html
+
+  // failed login ==> try agai
+    // if failed 'x' times lock out???
+
+
+// Forgot Password
