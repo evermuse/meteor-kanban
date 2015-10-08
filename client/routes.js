7abe812f6839ff4e553573a819432cf36de6ac66
ndunhourowens <ndunhour@hotmail.com>
2015-10-07 18:07:15 -1000

    create three pages, index, signin, and dashboard

--- client/routes.js
+++ client/routes.js
@@ -7,6 +7,15 @@ Router.configure({
 
 Router.route('/', function() {
   //this is the section where we want stuff to go into yield
+  //first initial page
   this.render('index');
 
-})
+});
+
+Router.route('/sign_in', function() {
+  this.render('signIn');
+});
+
+Router.route('/dashboard', function() {
+  this.render('dashboard');
+});
