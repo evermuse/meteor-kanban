9ae6ba624bf08317d827d487614f1b15a4625256
Bryan Alexander <bryanalexander@me.com>
2015-10-09 11:32:29 -1000

    refactored taks columns into own files. added icebox functionality. more styling

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -2,10 +2,10 @@ Template.dashboard.events({
 
   // eventname  selector
 
-
   'submit #addTaskForm' : function(evt, template) { // template is blaze template instance.  blaze is the renedering engine
     evt.preventDefault(); // prevents page from reloading
 
+    //inserts the tasks into the db
     Tasks.insert({
 
       title : evt.target.title.value,
@@ -15,6 +15,7 @@ Template.dashboard.events({
 
     });
 
+    //clears the input boxes
     title: evt.target.title.value = '';
     description: evt.target.description.value = '';
   },
@@ -27,43 +28,25 @@ Template.dashboard.events({
     });
   },
 
+  //space to code the description/button hide feature
   'click #taskItem' : function(evt, template){
     evt.preventDefault();
   },
 
+  //move taskItem to next column
   'click .next' : function(evt, template){
     evt.preventDefault();
-    console.log(this);
     var newStatus = this.status;
     newStatus++;
-    Tasks.update(this._id, {$set: {status: newStatus}});
+    Tasks.update(this._id, {$set: {status: newStatus}}); //could be refactored to increment $inc >> http://docs.mongodb.org/manual/reference/operator/update/inc/
   },
+
+  //move item to previous column
   'click .previous' : function(evt, template){
     evt.preventDefault();
-    console.log(this);
     var newStatus = this.status;
     newStatus--;
     Tasks.update(this._id, {$set: {status: newStatus}});
   }
+
 });
-
-
-
-Template.dashboard.helpers({
-  getToDoAllItems: function(){
-    return Tasks.find({status: 1});
-  }
-});
-
-Template.dashboard.helpers({
-  getInProgressAllItems: function(){
-    return Tasks.find({status: 2});
-  }
-});
-
-Template.dashboard.helpers({
-  getCompletedAllItems: function(){
-    return Tasks.find({status: 3});
-  }
-});
-
